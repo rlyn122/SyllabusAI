3d511121bd907c883254591466ba33504f28f96b
Ryan Lin <125698385+rlyn122@users.noreply.github.com>
2024-01-04 11:36:53 -0800

    Untrack config.js

--- mern/server/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 3000;
-const PASSWORD = 0
-export const URL = `mongodb+srv://ryanlin232:${PASSWORD}@cluster0.5hwhijx.mongodb.net/syllabus?retryWrites=true&w=majority`
-export const APIKEY = `testtestgitignore works?`
