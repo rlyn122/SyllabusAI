72250c58719a1bdea5201d0441bf6a5a68672174
rlyn122 <125698385+rlyn122@users.noreply.github.com>
2024-01-09 10:02:00 -0800

    using multer to test receiving files on serverside

--- mern/server/routes/syllabusRoute.js
+++ mern/server/routes/syllabusRoute.js
@@ -1,32 +1,55 @@
 import express from 'express';
 import { SyllabusData } from '../models/syllabusModel.js';
+import multer from 'multer';
 
 const router = express.Router();
 
-//posting pdfs
-router.post('/upload', async (request, response) => {
-    try {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/")
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
 
-      if (!request.body.text) 
-      {
-        return response.status(400).send({
-          message: 'Send all required fields: text',
-        });
-      }
+const uploadStorage = multer({ storage: storage })
 
-      const newSyllabus = {
-        text: request.body.text
-      };
+// Single file
+router.post("/upload", uploadStorage.single("file"), (req, res) => {
+  console.log(req.file)
+  return res.send("Single file")
+})
+
+//Multiple files
+router.post("/upload/multiple", uploadStorage.array("file", 10), (req, res) => {
+  console.log(req.files)
+  return res.send("Multiple files")
+})
+
+// //posting pdfs
+// router.post('/upload', async (request, response) => {
+//     try {
+//       if (!request.body.text) 
+//       {
+//         return response.status(400).send({
+//           message: 'Send all required fields: text',
+//         });
+//       }
+
+//       const newSyllabus = {
+//         text: request.body.text
+//       };
   
-      const syllabus = await SyllabusData.create(newSyllabus);
+//       const syllabus = await SyllabusData.create(newSyllabus);
   
-      return response.status(201).send(syllabus);
+//       return response.status(201).send(syllabus);
 
-    } catch (error) {
-      console.log(error.message);
-      response.status(500).send({ message: error.message });
-    }
-  });
+//     } catch (error) {
+//       console.log(error.message);
+//       response.status(500).send({ message: error.message });
+//     }
+//   });
 
 // Route for Get All Books from database
 router.get('/', async (request, response) => {
